0b7103b51d69eb9efd4ea6b3430d3d1a2b2096fe
gherold <gherold@me.com>
2026-02-04 16:44:15 +0100

    fix(iOS): use native share sheet for PDF download
    - Install @capacitor/share and @capacitor/filesystem
    - Write PDF to cache, then share via native share sheet
    - Allows saving to Files, AirDrop, email, etc. on iOS

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,7 +12,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.2"),
-        .package(name: "CapacitorCommunityNativeAudio", path: "../../../node_modules/@capacitor-community/native-audio")
+        .package(name: "CapacitorCommunityNativeAudio", path: "../../../node_modules/@capacitor-community/native-audio"),
+        .package(name: "CapacitorFilesystem", path: "../../../node_modules/@capacitor/filesystem"),
+        .package(name: "CapacitorShare", path: "../../../node_modules/@capacitor/share")
     ],
     targets: [
         .target(
@@ -20,7 +22,9 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
-                .product(name: "CapacitorCommunityNativeAudio", package: "CapacitorCommunityNativeAudio")
+                .product(name: "CapacitorCommunityNativeAudio", package: "CapacitorCommunityNativeAudio"),
+                .product(name: "CapacitorFilesystem", package: "CapacitorFilesystem"),
+                .product(name: "CapacitorShare", package: "CapacitorShare")
             ]
         )
     ]
